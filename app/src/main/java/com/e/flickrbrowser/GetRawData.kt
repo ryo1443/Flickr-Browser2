2142b6f949ccc19cbc089680af59350eed513e63
ryo <ryo12271443@icloud.com>
2020-07-03 23:37:55 +0900

    ダウンロード完了のコールバックまで実装

---
+++ app/src/main/java/com/e/flickrbrowser/GetRawData.kt
@@ -0,0 +1,67 @@
+package com.e.flickrbrowser
+
+import android.net.sip.SipSession
+import android.net.wifi.p2p.WifiP2pManager
+import android.os.AsyncTask
+import android.util.Log
+import java.io.IOException
+import java.lang.Exception
+import java.net.MalformedURLException
+import java.net.URL
+
+enum class DownloadStatus {
+    OK, IDLE, NOT_INITIALIZED, FAILED_OR_EMPTY, PERMISSIONS_ERROR, ERROR
+}
+
+class GetRawData(private val listener: OnDownloadComplete) : AsyncTask<String, Void, String>() {
+    private val TAG = "GetRawData"
+    private var downloadStatus = DownloadStatus.IDLE
+
+    interface OnDownloadComplete {
+        fun onDownloadComplete(data: String, status: DownloadStatus)
+    }
+
+//    private var listener: MainActivity? = null
+//
+//    fun setDownloadCompleteListener(callbackObject: MainActivity) {
+//        listener = callbackObject
+//    }
+
+    override fun onPostExecute(result: String) {
+        Log.d(TAG, "onPostExecute called, parameter is $result")
+        listener.onDownloadComplete(result, downloadStatus)
+    }
+
+    override fun doInBackground(vararg params: String?): String {
+        if (params[0] == null) {
+            downloadStatus = DownloadStatus.NOT_INITIALIZED
+            return "No URL specified"
+        }
+
+        try {
+            downloadStatus = DownloadStatus.OK
+            return URL(params[0]).readText()
+        } catch (e: Exception) {
+            val errorMessage = when (e) {
+                is MalformedURLException -> {
+                    downloadStatus = DownloadStatus.NOT_INITIALIZED
+                    "doInBackgrond: Invalid URL ${e.message}"
+                }
+                is IOException -> {
+                    downloadStatus = DownloadStatus.FAILED_OR_EMPTY
+                    "doInBackgrond: IO Exception reading data: ${e.message}"
+                }
+                is SecurityException -> {
+                    downloadStatus = DownloadStatus.PERMISSIONS_ERROR
+                    "doInBackgrond: Security exception: Needs permission? ${e.message}"
+                } else -> {
+                    downloadStatus = DownloadStatus.ERROR
+                    "Unknown error: ${e.message}"
+                }
+            }
+            Log.e(TAG, errorMessage)
+
+            return errorMessage
+        }
+    }
+}
